606492860bd64a80b539bb5903dfc40ddc9481d4
yang sun <yangsun@yangs-MacBook-Pro.local>
2020-07-04 19:59:10 -0700

    update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
 
 @app.route("/scrape")
 def scraper():
-    mars = db.mars
+    # mars = db.mars
     mars_data = scrape_mars.scrape()
     mars.update({},mars_data, upsert = True)
     return redirect("/",code=302)
