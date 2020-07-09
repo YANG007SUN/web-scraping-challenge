e5b4b9e257d48f5283dcb10f3b9df3f8b6029df7
yang sun <yangsun@yangs-MacBook-Pro.local>
2020-07-08 20:01:10 -0700

    ddd

--- scrape_mars.py
+++ scrape_mars.py
@@ -136,7 +136,7 @@ def scrape():
         summary_dict = dict(news_title = news_title,
                             news_p = news_p,
                             featured_image = featured_image,
-                            mars_weather =mars_weather[0],
+                            mars_weather =mars_weather,
                             fact_table = fact_table_html,
                             hemisphere_image_urls = hemisphere_image_urls)
 
